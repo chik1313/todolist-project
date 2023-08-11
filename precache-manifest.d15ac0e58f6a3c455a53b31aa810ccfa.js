1e27f04b1fa8bb90dec458795a67a087ad5b3cc4
chik <ochkurov96@gmail.com>
2023-08-11 16:31:34 +0300

    Updates

---
+++ precache-manifest.d15ac0e58f6a3c455a53b31aa810ccfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f12b268f02809be8bba304583580b28",
+    "url": "/todolist-project/index.html"
+  },
+  {
+    "revision": "de26a0adf6b534f58728",
+    "url": "/todolist-project/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/todolist-project/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist-project/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de26a0adf6b534f58728",
+    "url": "/todolist-project/static/js/main.eda50f62.chunk.js"
+  },
+  {
+    "revision": "338e58d5d59766aefcca",
+    "url": "/todolist-project/static/js/runtime-main.b6427cb7.js"
+  }
+]);
